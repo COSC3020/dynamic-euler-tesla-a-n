c0d0188cfc9672b68f3656ccca8a17ff02bb7593
tesla-a-n <143447224+tesla-a-n@users.noreply.github.com>
2025-05-12 22:49:58 -0600

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -3,4 +3,11 @@ const assert = require('assert');
 eval(fs.readFileSync('code.js')+'');
 
 assert(factorial(10) === 3628800);
+assert(factorial(100) === 9.33262154439441e+157);
+assert(factorial(0) === 1);
 assert(e(10) === 2.7182818011463845);
+assert(e(100) === 2.7182818284590455);
+assert(e(0) === 1);
+
+//we can also test how the functions work together
+assert(2 + 1.0 / factorial(2) === e(2));
